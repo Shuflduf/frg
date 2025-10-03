b2ea673edd6fde0e4d87477fad0ebf07cd3906ec
Shuflduf <shuflduf@gmail.com>
2025-10-03 01:37:54 -0600

    defining tokens

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,46 @@
 use std::env;
 
+#[derive(Debug, PartialEq, Clone)]
+enum Keyword {
+    Struct,
+    Void,
+    Int,
+    Str,
+    Vec,
+    Map,
+    Set,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum Symbol {
+    LeftBrace,
+    RightBrace,
+    LeftParen,
+    RightParen,
+    LeftBracket,
+    RightBracket,
+    Equals,
+    Comma,
+    Ampersand,
+    Star,
+    Colon,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum Literal {
+    Int(i32),
+    Float(f32),
+    String(String),
+    Identifier(String),
+}
+
+#[derive(Debug, Clone)]
+enum Token {
+    Keyword(Keyword),
+    Symbol(Symbol),
+    Literal(Literal),
+}
+
 fn main() {
     let args = env::args().collect::<Vec<String>>();
     if args.len() < 2 {
