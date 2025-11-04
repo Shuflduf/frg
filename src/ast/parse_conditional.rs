03d3f435699857d2b358f8a6d7c98c08ec7d2633
Shuflduf <shuflduf@gmail.com>
2025-11-03 22:26:41 -0700

    lmao

--- src/ast/parse_conditional.rs
+++ src/ast/parse_conditional.rs
@@ -3,17 +3,7 @@ use super::*;
 pub fn parse(token_iter: &mut Peekable<Iter<Token>>) -> Statement {
     expect_token(token_iter, Token::Keyword(lexer::Keyword::If));
     let value = parse_expression(token_iter);
-    expect_symbol(token_iter, lexer::Symbol::LeftBrace);
-    let mut body = vec![];
-
-    while let Some(new_node) = parse_next(token_iter) {
-        dbg!(&new_node);
-        body.push(new_node);
-        if token_iter.peek() == Some(&&Token::Symbol(lexer::Symbol::RightBrace)) {
-            token_iter.next();
-            break;
-        }
-    }
+    let body = parse_function::parse_body(token_iter);
 
     Statement::Conditional { value, body }
 }
