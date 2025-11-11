e66a47fd9a4d37805020489cfd111ae7ed60b0ba
Shuflduf <shuflduf@gmail.com>
2025-11-11 01:55:04 -0700

    no mangle

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
 pub mod ast;
 pub mod interpreter;
 pub mod lexer;
+
+#[unsafe(no_mangle)]
+pub fn evaluate_frg(source_code: String) -> String {
+    let tokens = lexer::lex(&source_code);
+    let ast = ast::parse(tokens);
+    let result = interpreter::interpret(ast);
+
+    result
+        .unwrap_or(interpreter::context::VariableValue::Void)
+        .to_string()
+}
