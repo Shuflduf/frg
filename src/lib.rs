1a82c736f528c03e0a09d9a47a3fb537e3ce1d0d
Shuflduf <shuflduf@gmail.com>
2025-11-13 11:26:30 -0700

    IT FUCKIN WORKS

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,16 @@
-use std::ffi::{CStr, CString, c_char};
+use wasm_bindgen::prelude::wasm_bindgen;
 
 pub mod ast;
 pub mod interpreter;
 pub mod lexer;
 
-#[unsafe(no_mangle)]
-pub extern "C" fn evaluate_frg(source_code: *const c_char) -> *mut c_char {
-    let source_code = unsafe { CStr::from_ptr(source_code) }.to_str().unwrap();
-    let tokens = lexer::lex(source_code);
+#[wasm_bindgen]
+pub fn evaluate_frg(source_code: String) -> String {
+    let tokens = lexer::lex(&source_code);
     let ast = ast::parse(tokens);
     let result = interpreter::interpret(ast);
 
-    let str_result = result
+    result
         .unwrap_or(interpreter::context::VariableValue::Void)
-        .to_string();
-
-    CString::new(str_result).unwrap().into_raw()
+        .to_string()
 }
