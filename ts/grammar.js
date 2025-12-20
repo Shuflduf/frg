e023fbfeb44a668e53b426d274e698315ba9c499
Shuflduf <shuflduf@gmail.com>
2025-12-19 17:47:51 -0700

    maps and sets and emptys, something i couldnt fucking do last time

--- ts/grammar.js
+++ ts/grammar.js
@@ -10,6 +10,11 @@
 module.exports = grammar({
   name: "frg",
 
+  conflicts: ($) => [
+    [$.set_literal, $.map_literal, $.empty_collection],
+    [$.set_literal, $.map_literal],
+  ],
+
   rules: {
     source_file: ($) => repeat($._statement),
 
@@ -17,9 +22,12 @@ module.exports = grammar({
 
     variable_declaration: ($) => seq($.type, $.identifier, "=", $.expression),
 
-    type: ($) => choice("int", "float", "str", "bool", $.vec_type),
+    type: ($) =>
+      choice("int", "float", "str", "bool", $.vec_type, $.set_type, $.map_type),
 
     vec_type: ($) => seq("vec", "(", $.type, ")"),
+    set_type: ($) => seq("set", "(", $.type, ")"),
+    map_type: ($) => seq("map", "(", $.type, repeat(","), $.type, ")"),
 
     identifier: ($) => /[a-zA-Z_][a-zA-Z0-9_]*/,
 
@@ -30,6 +38,9 @@ module.exports = grammar({
         $.float_literal,
         $.string_literal,
         $.vec_literal,
+        $.map_literal,
+        $.set_literal,
+        $.empty_collection,
       ),
 
     binary_expression: ($) =>
@@ -39,11 +50,15 @@ module.exports = grammar({
       ),
 
     number_literal: ($) => /\d+/,
-
     float_literal: ($) => /\d+\.\d+/,
-
     string_literal: ($) => seq('"', /[^"]*/, '"'),
 
     vec_literal: ($) => seq("[", repeat(choice($.expression, ",")), "]"),
+    set_literal: ($) => seq("{", repeat(choice($.expression, ",")), "}"),
+    map_literal: ($) => seq("{", repeat(choice($.map_entry, ",")), "}"),
+
+    empty_collection: ($) => seq("{", repeat(","), "}"),
+
+    map_entry: ($) => seq($.expression, repeat1(":"), $.expression),
   },
 });
