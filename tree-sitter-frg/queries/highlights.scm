cc156872bef6d3ef317fe1de4931374c75f24298
Shuflduf <shuflduf@gmail.com>
2026-04-14 00:28:01 -0600

    syntax highlighting works now ig but its ugly as fuck

--- tree-sitter-frg/queries/highlights.scm
+++ tree-sitter-frg/queries/highlights.scm
@@ -1,15 +1,28 @@
 (comment) @comment
 
-(type) @type
 "void" @type
 "int" @type
 "float" @type
 "str" @type
 "bool" @type
 "vec" @type
-"set" @type
+"arr" @type
+"array" @type
+"list" @type
 "map" @type
+"obj" @type
+"hashmap" @type
+"dict" @type
+"dictionary" @type
 "struct" @type
+; "set" @type
+
+(type) @type
+(vec_type) @type
+(map_type) @type
+(function_type) @type
+(reference_type) @type
+(struct_identifier) @type
 
 (int_literal) @number
 (float_literal) @number
@@ -17,6 +30,24 @@
 (bool_literal) @boolean
 
 (identifier) @variable
+(num_identifier) @variable
+
+(vec_literal) @attribute
+(map_literal) @attribute
+(empty_collection) @attribute
+
+(function_literal) @attribute
+(function_call) @attribute
+(builtin) @attribute
+
+"struct" @keyword
+"return" @keyword
+"if" @keyword
+"else" @keyword
+
+(if_statement) @conditional
+(else_if_statement) @conditional
+(else_statement) @conditional
 
 "=" @operator
 "+" @operator
@@ -50,9 +81,9 @@
 ; (function_call (identifier) @function.call)
 (function_literal) @function
 
-"if" @conditional
-"else" @conditional
-"return" @keyword
+; "if" @conditional
+; "else" @conditional
+; "return" @keyword
 
 (builtin) @function.builtin
-"@" @function.builtin
+; "@" @function.builtin
